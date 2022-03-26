3376a24611c367b28c37417fc4813d951124ff92
christseng <tzuhsi.tseng@gmail.com>
2022-03-26 15:25:03 +0800

    feat: resolve the problem that check if the integer has alternating bits

--- binary-number-with-alternating-bits/main.go
+++ binary-number-with-alternating-bits/main.go
@@ -1,6 +1,26 @@
 package main
 
+import (
+	"fmt"
+)
+
+// https://leetcode.com/problems/binary-number-with-alternating-bits/
+
 func hasAlternatingBits(n int) bool {
-	n = (n >> 1) ^ n
-	return n&(n+1) == 0
+	m := n ^ n>>1       // m should be all of 1 bits if n has alternating bits (e.g. 101 ^ 010)
+	return m&(m+1) == 0 // m+1 should be all of 0 bits except the first bit
+}
+
+//func hasAlternatingBits(n int) bool {
+//	b := strconv.FormatInt(int64(n), 2)
+//	for i := 0; i < len(b)-1; i++ {
+//		if b[i] == b[i+1] {
+//			return false
+//		}
+//	}®
+//	return true
+//}
+
+func main() {
+	fmt.Println(hasAlternatingBits(5))
 }
