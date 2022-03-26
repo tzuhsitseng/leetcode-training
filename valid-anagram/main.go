42915a7a9413ce617d97e43a262ff3e1951e10d3
christseng <tzuhsi.tseng@gmail.com>
2022-03-26 14:16:04 +0800

    feat: resolve the problem that checking if two string are anagram

--- valid-anagram/main.go
+++ valid-anagram/main.go
@@ -1,44 +1,37 @@
 package main
 
-// Keyword: hash table
+import "fmt"
+
+// https://leetcode.com/problems/valid-anagram/
 
 func isAnagram(s string, t string) bool {
-	counter := map[string]int{}
-
-	// 若長度就不一樣了就肯定不是 anagram
 	if len(s) != len(t) {
 		return false
 	}
 
-	for _, val := range s {
-		// 將 rune 先轉成 string
-		character := string(val)
+	counter := map[rune]int{}
 
-		// 若有找到就 count ++, 若沒有就直接從頭開始 = 1
-		if _, ok := counter[character]; !ok {
-			counter[character] = 1
+	for _, c := range s {
+		if _, ok := counter[c]; ok {
+			counter[c]++
 		} else {
-			counter[character] += 1
+			counter[c] = 1
 		}
 	}
 
-	for _, val := range t {
-		// 將 rune 先轉成 string
-		character := string(val)
-
-		v, ok := counter[character]
-		// 如果 counter 找不到或是已經 count = 0 了，就代表不是 anagram
-		if !ok || v == 0 {
+	for _, c := range t {
+		if _, ok := counter[c]; !ok {
 			return false
 		}
-
-		// 若 count 已經歸零, 就直接 delete, 以利最終可以用長度判斷是否為 anagram
-		// 否則就 count --
-		if v-1 == 0 {
-			delete(counter, character)
-		} else {
-			counter[character] = v - 1
+		counter[c]--
+		if counter[c] == 0 {
+			delete(counter, c)
 		}
 	}
+
 	return len(counter) == 0
+}
+
+func main() {
+	fmt.Println(isAnagram("anagram", "nagaram"))
 }
