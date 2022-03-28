9728d4090c7289b7447a87a43c5e37a195a54668
christseng <tzuhsi.tseng@gmail.com>
2022-03-29 00:39:08 +0800

    feat: resolve the problem that merging two linked list

--- merge-two-sorted-lists/main.go
+++ merge-two-sorted-lists/main.go
@@ -14,12 +14,11 @@ type ListNode struct {
 }
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	temp := &ListNode{}
-	iter := temp
+	tmp := &ListNode{}
+	iter := tmp
 
-	// 若 l1 跟 l2 都還有節點就繼續作比較
 	for l1 != nil && l2 != nil {
-		if l1.Val < l2.Val {
+		if l1.Val <= l2.Val {
 			iter.Next = l1
 			l1 = l1.Next
 		} else {
@@ -29,12 +28,11 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 		iter = iter.Next
 	}
 
-	// 若 l1 還有節點，但 l2 已經空了，就直接串上 l1
-	// 反之，若 l2 還有節點，但 l1 已經空了，就直接串上 l2
 	if l1 != nil {
 		iter.Next = l1
 	} else if l2 != nil {
 		iter.Next = l2
 	}
-	return temp.Next
+
+	return tmp.Next
 }
