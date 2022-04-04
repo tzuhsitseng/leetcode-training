07a37549692b99778bda7eaf97a1ac434cb00cbe
christseng <tzuhsi.tseng@gmail.com>
2022-04-04 14:32:27 +0800

    feat: resolve the problem that checking whether the tree is balanced

--- balanced-binary-tree/main.go
+++ balanced-binary-tree/main.go
@@ -18,22 +18,20 @@ type TreeNode struct {
 }
 
 func isBalanced(root *TreeNode) bool {
-	return depth(root) != -1
+	return getDepth(root) != -1
 }
 
-func depth(node *TreeNode) int {
+func getDepth(node *TreeNode) int {
 	if node == nil {
 		return 0
 	}
 
-	l := depth(node.Left)
-	r := depth(node.Right)
+	left := getDepth(node.Left)
+	right := getDepth(node.Right)
 
-	// 若發現深度已經不匹配或是深度差異大於 1 了，就回傳 -1 代表 unbalanced
-	if l == -1 || r == -1 || math.Abs(float64(l-r)) > 1 {
+	if left == -1 || right == -1 || math.Abs(float64(left-right)) > 1 {
 		return -1
 	}
 
-	// 否則深度就是自己 + 左右子樹 max depth
-	return 1 + int(math.Max(float64(l), float64(r)))
+	return 1 + int(math.Max(float64(left), float64(right)))
 }
