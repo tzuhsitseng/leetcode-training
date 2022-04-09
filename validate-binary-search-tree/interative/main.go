f338e79fae69f7050e4f44c97bd2ba7018f8a24d
christseng <tzuhsi.tseng@gmail.com>
2022-04-09 14:25:37 +0800

    feat: resolve the problem of BST validation

--- validate-binary-search-tree/interative/main.go
+++ validate-binary-search-tree/interative/main.go
@@ -1,13 +1,8 @@
 package main
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
+import "fmt"
+
+// https://leetcode.com/problems/validate-binary-search-tree/
 
 type TreeNode struct {
 	Val   int
@@ -15,37 +10,99 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+type TreeNodeStack struct {
+	stack []*TreeNode
+}
+
+func NewTreeNodeStack() *TreeNodeStack {
+	return &TreeNodeStack{
+		stack: make([]*TreeNode, 0),
+	}
+}
+
+func (s *TreeNodeStack) push(element *TreeNode) {
+	if s.stack == nil {
+		s.stack = make([]*TreeNode, 0)
+	}
+	s.stack = append(s.stack, element)
+}
+
+func (s *TreeNodeStack) pop() *TreeNode {
+	if s.isEmpty() {
+		return nil
+	}
+
+	l := len(s.stack)
+	result := s.stack[l-1]
+	s.stack = s.stack[:l-1]
+	return result
+}
+
+func (s *TreeNodeStack) isEmpty() bool {
+	return len(s.stack) == 0
+}
+
+func (s *TreeNodeStack) peek() *TreeNode {
+	if s.isEmpty() {
+		return nil
+	}
+
+	return s.stack[len(s.stack)-1]
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	result := make([]int, 0)
+	stack := NewTreeNodeStack()
+	cur := root
+
+	for cur != nil || !stack.isEmpty() {
+		for cur != nil {
+			stack.push(cur)
+			cur = cur.Left
+		}
+
+		cur = stack.pop()
+
+		if cur != nil {
+			result = append(result, cur.Val)
+			cur = cur.Right
+		}
+	}
+
+	return result
+}
+
 func isValidBST(root *TreeNode) bool {
 	if root == nil {
-		return true
+		return false
 	}
 
-	var pre *TreeNode
+	var prev *TreeNode
 	curr := root
-	stack := make([]*TreeNode, 0)
+	stack := NewTreeNodeStack()
 
-	for curr != nil || len(stack) > 0 {
+	for curr != nil || !stack.isEmpty() {
 		for curr != nil {
-			stack = push(stack, curr)
+			stack.push(curr)
 			curr = curr.Left
 		}
-		curr, stack = pop(stack)
-		if pre != nil && pre.Val >= curr.Val {
+
+		curr = stack.pop()
+		if prev != nil && curr.Val <= prev.Val {
 			return false
 		}
-		curr, pre = curr.Right, curr
+
+		prev, curr = curr, curr.Right
 	}
+
 	return true
 }
 
-func push(stack []*TreeNode, elements ...*TreeNode) []*TreeNode {
-	return append(stack, elements...)
-}
-
-func pop(stack []*TreeNode) (*TreeNode, []*TreeNode) {
-	l := len(stack)
-	if l > 0 {
-		return stack[l-1], stack[:l-1]
-	}
-	return nil, nil
+func main() {
+	root := &TreeNode{Val: 0}
+	fmt.Println(isValidBST(root))
 }
--- validate-binary-search-tree/recursive/main.go
+++ validate-binary-search-tree/recursive/main.go
@@ -2,14 +2,7 @@ package main
 
 import "math"
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
+// https://leetcode.com/problems/validate-binary-search-tree/
 
 type TreeNode struct {
 	Val   int
@@ -18,13 +11,15 @@ type TreeNode struct {
 }
 
 func isValidBST(root *TreeNode) bool {
-	return valid(root, math.Inf(-1), math.Inf(1))
+	return validBST(root, math.Inf(-1), math.Inf(1))
 }
 
-func valid(root *TreeNode, min, max float64) bool {
+func validBST(root *TreeNode, min, max float64) bool {
 	if root == nil {
 		return true
 	}
+
 	v := float64(root.Val)
-	return v < max && v > min && valid(root.Left, min, v) && valid(root.Right, v, max)
+
+	return v > min && v < max && validBST(root.Left, min, v) && validBST(root.Right, v, max)
 }
