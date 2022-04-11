86a838e323db87ac260f987254118055f25b8a47
christseng <tzuhsi.tseng@gmail.com>
2022-04-11 18:06:27 +0800

    feat: resolve the problem that calc. the longest palindrome

--- longest-palindrome/main.go
+++ longest-palindrome/main.go
@@ -1,26 +1,27 @@
 package main
 
-// Keyword: bucket sort
+// https://leetcode.com/problems/longest-palindrome/
 
 func longestPalindrome(s string) int {
-	p := 0
-	cnt := make([]int, 128)
-	for _, v := range s {
-		cnt[int(v)] += 1
+	result := 0
+	odd := false
+	counter := map[rune]int{}
+
+	for _, c := range s {
+		counter[c]++
 	}
 
-	// 64-91 -> A-Z
-	// n+32 -> a-z
-	for i := 64; i < 91; i++ {
-		p += cnt[i]/2*2 + cnt[i+32]/2*2
-	}
+	for _, v := range counter {
+		result += v / 2 * 2
 
-	// 確認是否有落單值可以填補中間位的
-	for i := 64; i < 91; i++ {
-		if cnt[i]%2 == 1 || cnt[i+32]%2 == 1 {
-			p += 1
-			break
+		if !odd && v%2 != 0 {
+			odd = true
 		}
 	}
-	return p
+
+	if odd {
+		result += 1
+	}
+
+	return result
 }
