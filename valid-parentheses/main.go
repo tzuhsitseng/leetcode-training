426c33c39198467618c3a1fc90861c93fa818a93
christseng <tzuhsi.tseng@gmail.com>
2022-04-19 14:24:18 +0800

    feat: resolve the problem that checking the string is parentheses

--- valid-parentheses/main.go
+++ valid-parentheses/main.go
@@ -1,44 +1,75 @@
 package main
 
-func isValid(s string) bool {
-	mapping := map[string]string{
-		"}": "{",
-		"]": "[",
-		")": "(",
+// https://leetcode.com/problems/valid-parentheses/
+
+type StringStack struct {
+	stack []string
+}
+
+func NewStringStack() *StringStack {
+	return &StringStack{
+		stack: make([]string, 0),
 	}
+}
+
+func (s *StringStack) push(element string) {
+	if s.stack == nil {
+		s.stack = make([]string, 0)
+	}
+	s.stack = append(s.stack, element)
+}
+
+func (s *StringStack) pop() *string {
+	if s.isEmpty() {
+		return nil
+	}
+
+	l := len(s.stack)
+	result := s.stack[l-1]
+	s.stack = s.stack[:l-1]
+	return &result
+}
+
+func (s *StringStack) isEmpty() bool {
+	return len(s.stack) == 0
+}
+
+func (s *StringStack) peek() *string {
+	if s.isEmpty() {
+		return nil
+	}
+
+	return &s.stack[len(s.stack)-1]
+}
+
+func isValid(s string) bool {
 	if len(s) == 0 {
 		return true
 	}
-	stack := make([]string, 0)
 
-	for _, v := range s {
-		c := string(v)
+	if len(s)%2 != 0 {
+		return false
+	}
 
-		if c == "{" || c == "[" || c == "(" {
-			stack = push(stack, c)
-		} else if c == "}" || c == "]" || c == ")" {
-			var temp string
-			temp, stack = pop(stack)
-			if temp != mapping[c] {
+	brackets := map[string]uint8{
+		"(": ')',
+		"{": '}',
+		"[": ']',
+	}
+
+	stack := NewStringStack()
+
+	for i := 0; i < len(s); i++ {
+		if _, ok := brackets[string(s[i])]; ok {
+			stack.push(string(s[i]))
+		} else {
+			left := stack.pop()
+			right := s[i]
+			if left == nil || brackets[*left] != right {
 				return false
 			}
 		}
 	}
 
-	if len(stack) > 0 {
-		return false
-	}
-	return true
-}
-
-func push(stack []string, elements ...string) []string {
-	return append(stack, elements...)
-}
-
-func pop(stack []string) (string, []string) {
-	l := len(stack)
-	if l > 0 {
-		return stack[l-1], stack[:l-1]
-	}
-	return "", nil
+	return stack.isEmpty()
 }
