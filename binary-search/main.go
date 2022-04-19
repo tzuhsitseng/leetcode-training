4b743a1f61e9601845f9403086eb5cba725d304d
christseng <tzuhsi.tseng@gmail.com>
2022-04-19 15:27:09 +0800

    feat: resolve the problem that doing binary search

--- binary-search/main.go
+++ binary-search/main.go
@@ -1,16 +1,28 @@
 package main
 
-func search(nums []int, target int) int {
-	min, max := 0, len(nums)-1
+// https://leetcode.com/problems/binary-search/
 
-	for max >= min {
-		mid := (max + min) / 2
-		if nums[mid] == target {
-			return mid
-		} else if nums[mid] > target {
-			max = mid - 1
-		} else if nums[mid] < target {
-			min = mid + 1
+func search(nums []int, target int) int {
+	size := len(nums)
+
+	if size == 0 {
+		return -1
+	}
+
+	if size == 1 && nums[0] == target {
+		return 0
+	}
+
+	l, r := 0, size-1
+
+	for r >= l {
+		m := (r-l)/2 + l
+		if target == nums[m] {
+			return m
+		} else if target > nums[m] {
+			l = m + 1
+		} else {
+			r = m - 1
 		}
 	}
 
