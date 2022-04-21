a57b3e781e3bdca600d8b3d8b352758aabd6bc2f
christseng <tzuhsi.tseng@gmail.com>
2022-04-21 21:16:42 +0800

    feat: resolve the problem that playing the game for last stone weight

--- last-stone-weight/main.go
+++ last-stone-weight/main.go
@@ -2,51 +2,43 @@ package main
 
 import "container/heap"
 
-func main() {
-	lastStoneWeight([]int{2, 7, 4, 1, 8, 1})
+// https://leetcode.com/problems/last-stone-weight/
+
+type IntHeap []int
+
+func (h IntHeap) Len() int            { return len(h) }
+func (h IntHeap) Less(i, j int) bool  { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *IntHeap) Pop() interface{} {
+	size := len(*h)
+	res := (*h)[size-1]
+	*h = (*h)[:size-1]
+	return res
 }
 
 func lastStoneWeight(stones []int) int {
 	if len(stones) == 0 {
 		return 0
 	}
-
-	h := &IntHeap{}
-	heap.Init(h)
-	for _, stone := range stones {
-		heap.Push(h, -stone)
+	if len(stones) == 1 {
+		return stones[0]
 	}
 
-	for h.Len() > 0 {
-		if h.Len() == 1 {
-			return -(heap.Pop(h).(int))
-		}
-		p1 := heap.Pop(h).(int)
-		p2 := heap.Pop(h).(int)
-		if p2-p1 > 0 {
-			heap.Push(h, -(p2 - p1))
+	hp := IntHeap(stones)
+	heap.Init(&hp)
+
+	for hp.Len() > 1 {
+		p1 := heap.Pop(&hp).(int)
+		p2 := heap.Pop(&hp).(int)
+		if p1-p2 > 0 {
+			heap.Push(&hp, p1-p2)
 		}
 	}
+
+	if hp.Len() == 1 {
+		return hp[0]
+	}
+
 	return 0
-}
-
-// An IntHeap is a min-heap of ints.
-type IntHeap []int
-
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *IntHeap) Push(x interface{}) {
-	// Push and Pop use pointer receivers because they modify the slice's length,
-	// not just its contents.
-	*h = append(*h, x.(int))
-}
-
-func (h *IntHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
 }
