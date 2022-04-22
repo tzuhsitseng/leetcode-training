1f167113610f2647b7218e2ff8dce0a943c52cd7
christseng <tzuhsi.tseng@gmail.com>
2022-04-22 16:55:42 +0800

    feat: resolve the problem that doing plus one

--- plus-one/main.go
+++ plus-one/main.go
@@ -1,13 +1,14 @@
 package main
 
 func plusOne(digits []int) []int {
-	size := len(digits)
-	for i := size - 1; i >= 0; i-- {
+	for i := len(digits) - 1; i >= 0; i-- {
 		if digits[i] < 9 {
 			digits[i]++
 			return digits
 		}
 		digits[i] = 0
 	}
-	return append([]int{1}, digits...)
+
+	result := append([]int{1}, digits...)
+	return result
 }
