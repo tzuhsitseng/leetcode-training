e929791ac5c7e1f59c5a2ba0cfb5446cd51c1661
christseng <tzuhsi.tseng@gmail.com>
2022-04-30 12:06:06 +0800

    feat: resolve the problem that finding two elements that the sum is equal to target

--- two-sum-ii-input-array-is-sorted/main.go
+++ two-sum-ii-input-array-is-sorted/main.go
@@ -1,12 +1,24 @@
 package main
 
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
 func twoSum(numbers []int, target int) []int {
-	temp := map[int]int{}
-	for numIdx, v := range numbers {
-		if tempIdx, ok := temp[target-v]; ok {
-			return []int{tempIdx + 1, numIdx + 1}
-		}
-		temp[v] = numIdx
+	if len(numbers) == 0 || len(numbers) == 1 {
+		return nil
 	}
+
+	left, right := 0, len(numbers)-1
+
+	for left < right {
+		sum := numbers[left] + numbers[right]
+		if sum == target {
+			return []int{left + 1, right + 1}
+		} else if sum > target {
+			right--
+		} else {
+			left++
+		}
+	}
+
 	return nil
 }
