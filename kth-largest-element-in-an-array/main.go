928360b95822791a462159356b76493b5cd73159
christseng <tzuhsi.tseng@gmail.com>
2022-05-04 17:18:36 +0800

    feat: resolve the problem that finding the kth largest element in an array

--- kth-largest-element-in-an-array/main.go
+++ kth-largest-element-in-an-array/main.go
@@ -1,78 +1,92 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 )
 
 // https://leetcode.com/problems/kth-largest-element-in-an-array/
 
-func main() {
-	fmt.Println(findKthLargest([]int{3, 2, 1, 5, 6, 4}, 2))
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] > h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	l := h.Len()
+	res := (*h)[l-1]
+	*h = (*h)[:l-1]
+	return res
 }
 
 func findKthLargest(nums []int, k int) int {
-	if len(nums) == 0 {
-		return -1
-	}
+	h := IntHeap(nums)
+	heap.Init(&h)
 
-	if len(nums) == 1 {
-		return nums[0]
+	res := 0
+	for i := 0; i < k; i++ {
+		res = heap.Pop(&h).(int)
 	}
-
-	if k < 1 || k > len(nums) {
-		return -1
-	}
-
-	idx := len(nums) - k
-	return find(nums, idx)
+	return res
 }
 
-func find(nums []int, idx int) int {
-	l := make([]int, 0)
-	r := make([]int, 0)
-	m := len(nums) / 2
-	pivot := nums[m]
+// --
+// The following solution is quick sort
+// --
 
-	for i, v := range nums {
-		if v <= pivot && i != m {
-			l = append(l, v)
-		} else if v > pivot {
-			r = append(r, v)
-		}
-	}
-
-	if idx == len(l) {
-		return pivot
-	} else if idx < len(l) {
-		return find(l, idx)
-	} else {
-		return find(r, idx-1-len(l))
-	}
-}
-
-//func quickSort(nums []int, l, r int) {
-//	if r < l {
-//		return
+//func findKthLargest(nums []int, k int) int {
+//	if len(nums) == 0 {
+//		return -1
 //	}
 //
-//	left, right := l, r
-//	m := (r-l)/2 + l
+//	if len(nums) == 1 {
+//		return nums[0]
+//	}
+//
+//	if k < 1 || k > len(nums) {
+//		return -1
+//	}
+//
+//	idx := len(nums) - k
+//	return find(nums, idx)
+//}
+//
+//func find(nums []int, idx int) int {
+//	l := make([]int, 0)
+//	r := make([]int, 0)
+//	m := len(nums) / 2
 //	pivot := nums[m]
 //
-//	for right >= left {
-//		for right >= left && nums[left] < pivot {
-//			left++
-//		}
-//		for right >= left && nums[right] > pivot {
-//			right--
-//		}
-//		if right >= left {
-//			nums[left], nums[right] = nums[right], nums[left]
-//			left++
-//			right--
+//	for i, v := range nums {
+//		if v <= pivot && i != m {
+//			l = append(l, v)
+//		} else if v > pivot {
+//			r = append(r, v)
 //		}
 //	}
 //
-//	quickSort(nums, l, right)
-//	quickSort(nums, left, r)
+//	if idx == len(l) {
+//		return pivot
+//	} else if idx < len(l) {
+//		return find(l, idx)
+//	} else {
+//		return find(r, idx-1-len(l))
+//	}
 //}
+
+func main() {
+	fmt.Println(findKthLargest([]int{3, 2, 1, 5, 6, 4}, 2))
+}
