c95c35edd30defb4f59702ab189d69833f800572
christseng <tzuhsi.tseng@gmail.com>
2022-05-10 16:08:59 +0800

    feat: resolve the problem that robbing the maximum amount of money

--- house-robber/main.go
+++ house-robber/main.go
@@ -1,25 +1,27 @@
 package main
 
-import "math"
-
 // https://leetcode.com/problems/house-robber/
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
 
 func rob(nums []int) int {
 	size := len(nums)
-	if size == 0 {
-		return 0
-	}
 	if size == 1 {
 		return nums[0]
 	}
 
-	robbed := make([]int, size)
-	robbed[0] = nums[0]
-	robbed[1] = int(math.Max(float64(nums[0]), float64(nums[1])))
+	dp := make([]int, size)
+	dp[0] = nums[0]
+	dp[1] = max(dp[0], nums[1])
 
 	for i := 2; i < size; i++ {
-		robbed[i] = int(math.Max(float64(robbed[i-1]), float64(robbed[i-2]+nums[i])))
+		dp[i] = max(dp[i-1], dp[i-2]+nums[i])
 	}
 
-	return robbed[size-1]
+	return dp[size-1]
 }
