498056643c63092bd0aab0462f1622b466296642
christseng <tzuhsi.tseng@gmail.com>
2022-05-12 18:49:02 +0800

    feat: resolve the problem that finding all decode ways

--- decode-ways/main.go
+++ decode-ways/main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "strings"
 
 // https://leetcode.com/problems/decode-ways/
 
@@ -11,50 +8,28 @@ func numDecodings(s string) int {
 	dp := map[int]int{
 		len(s): 1,
 	}
-	//result := findByRecursive(s, 0, dp)
-	result := findByIterative(s, dp)
-	return result
-}
 
-func findByIterative(s string, dp map[int]int) int {
-	for i := len(s) - 1; i >= 0; i-- {
+	var dfs func(i int) int
+	dfs = func(i int) int {
+		res := 0
+
+		if v, ok := dp[i]; ok {
+			return v
+		}
+
 		if s[i] == '0' {
-			dp[i] = 0
-		} else {
-			dp[i] = dp[i+1]
+			return 0
 		}
 
+		res = dfs(i + 1)
 		if i < len(s)-1 &&
-			(s[i] == '1' ||
-				s[i] == '2' && strings.Index("0123456", string(s[i+1])) != -1) {
-			dp[i] += dp[i+2]
+			(s[i] == '1' || s[i] == '2' &&
+				strings.Index("0123456", string(s[i+1])) != -1) {
+			res += dfs(i + 2)
 		}
-	}
-	return dp[0]
-}
-
-func findByRecursive(s string, i int, dp map[int]int) int {
-	if v, ok := dp[i]; ok {
-		return v
+		dp[i] = res
+		return res
 	}
 
-	if s[i] == '0' {
-		return 0
-	}
-
-	result := findByRecursive(s, i+1, dp)
-
-	if i < len(s)-1 &&
-		(s[i] == '1' ||
-			s[i] == '2' && strings.Index("0123456", string(s[i+1])) != -1) {
-		result += findByRecursive(s, i+2, dp)
-	}
-
-	dp[i] = result
-
-	return result
-}
-
-func main() {
-	fmt.Println(numDecodings("226"))
+	return dfs(0)
 }
