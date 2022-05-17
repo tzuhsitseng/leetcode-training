2566642d94b84597753e4e07b433702e2b8d9aea
christseng <tzuhsi.tseng@gmail.com>
2022-05-17 16:24:43 +0800

    feat: resolve the problem that selecting top k frequent elements

--- top-k-frequent-elements/main.go
+++ top-k-frequent-elements/main.go
@@ -1,8 +1,38 @@
 package main
 
-import "sort"
+import "container/heap"
 
 // https://leetcode.com/problems/top-k-frequent-elements/
+
+type Element struct {
+	p int
+	v int
+}
+
+type PriorityQueue []*Element
+
+func (p PriorityQueue) Len() int {
+	return len(p)
+}
+
+func (p PriorityQueue) Less(i, j int) bool {
+	return p[i].p > p[j].p
+}
+
+func (p PriorityQueue) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func (p *PriorityQueue) Push(x interface{}) {
+	*p = append(*p, x.(*Element))
+}
+
+func (p *PriorityQueue) Pop() interface{} {
+	l := len(*p)
+	res := (*p)[l-1]
+	*p = (*p)[:l-1]
+	return res
+}
 
 func max(a, b int) int {
 	if a > b {
@@ -12,39 +42,26 @@ func max(a, b int) int {
 }
 
 func topKFrequent(nums []int, k int) []int {
-	counter := map[int]int{}
-	for _, v := range nums {
-		counter[v]++
+	m := map[int]int{}
+	for _, n := range nums {
+		m[n]++
 	}
 
-	elementsByCnt := map[int][]int{}
-	for k, v := range counter {
-		if _, ok := elementsByCnt[v]; !ok {
-			elementsByCnt[v] = []int{k}
-		} else {
-			elementsByCnt[v] = append(elementsByCnt[v], k)
-		}
+	s := make([]*Element, 0, len(m))
+	for val, counter := range m {
+		s = append(s, &Element{
+			p: counter,
+			v: val,
+		})
 	}
 
-	sorted := make([]int, 0, len(elementsByCnt))
-	for k := range elementsByCnt {
-		sorted = append(sorted, k)
+	p := PriorityQueue(s)
+	heap.Init(&p)
+
+	res := make([]int, 0, k)
+	for i := 0; i < k; i++ {
+		res = append(res, heap.Pop(&p).(*Element).v)
 	}
 
-	sort.Ints(sorted)
-
-	result := make([]int, 0)
-	total := 0
-
-	for i := len(sorted) - 1; i >= max(0, len(sorted)-k) && total < k; i-- {
-		elements := elementsByCnt[sorted[i]]
-		total += len(elements)
-		result = append(result, elements...)
-	}
-
-	return result
+	return res
 }
-
-// --
-// 可用 bubble sort 達到 O(n) 的效果
-// --
