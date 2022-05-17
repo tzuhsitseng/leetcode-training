78df3277ee416e092372bcca3f30298e383ab79a
christseng <tzuhsi.tseng@gmail.com>
2022-05-17 15:31:07 +0800

    feat: resolve the problem that checking whether the array has duplicate item

--- contain-duplicate/main.go
+++ contain-duplicate/main.go
@@ -3,18 +3,12 @@ package main
 // https://leetcode.com/problems/contains-duplicate/
 
 func containsDuplicate(nums []int) bool {
-	if len(nums) == 0 || len(nums) == 1 {
-		return false
-	}
-
-	counter := map[int]bool{}
-
-	for _, v := range nums {
-		if ok := counter[v]; ok {
+	m := map[int]bool{}
+	for i := range nums {
+		if m[nums[i]] {
 			return true
 		}
-		counter[v] = true
+		m[nums[i]] = true
 	}
-
 	return false
 }
