2a2c841d4b10f3ec357d3f07ce23c5751b32a347
christseng <tzuhsi.tseng@gmail.com>
2022-05-18 14:05:27 +0800

    feat: resolve the problem that checking whether the phrase is palidrome liked

--- valid-palindrome/main.go
+++ valid-palindrome/main.go
@@ -9,43 +9,28 @@ import (
 
 func isPalindrome(s string) bool {
 	reg, _ := regexp.Compile("^[a-z0-9]+$")
-
-	if len(s) == 0 {
-		return true
-	}
-
-	s = strings.ReplaceAll(s, " ", "")
 	s = strings.ToLower(s)
-
-	if len(s) == 0 || len(s) == 1 {
+	s = strings.Replace(s, " ", "", -1)
+	if s == "" {
 		return true
 	}
 
-	l := 0
-	r := len(s) - 1
-
-	for l < r {
+	l, r := 0, len(s)-1
+	for l <= r {
 		if !reg.Match([]byte{s[l]}) {
 			l++
 			continue
 		}
-
 		if !reg.Match([]byte{s[r]}) {
 			r--
 			continue
 		}
-
 		if s[l] != s[r] {
 			return false
 		}
-
 		l++
 		r--
 	}
 
 	return true
-}
-
-func main() {
-
 }
