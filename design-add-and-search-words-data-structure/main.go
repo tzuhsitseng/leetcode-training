b518c85e9795aa189e3a8c4bb5ab5135409ef01b
christseng <tzuhsi.tseng@gmail.com>
2022-05-21 00:43:24 +0800

    feat: resolve the problem that implement a special dictionary

--- design-add-and-search-words-data-structure/main.go
+++ design-add-and-search-words-data-structure/main.go
@@ -2,73 +2,67 @@ package main
 
 // https://leetcode.com/problems/design-add-and-search-words-data-structure/
 
+type TrieNode struct {
+	children map[rune]*TrieNode
+	end      bool
+}
+
 type WordDictionary struct {
 	root *TrieNode
 }
 
-type TrieNode struct {
-	nodes map[rune]*TrieNode
-}
-
 func Constructor() WordDictionary {
 	return WordDictionary{
-		root: &TrieNode{map[rune]*TrieNode{}},
+		root: &TrieNode{
+			children: map[rune]*TrieNode{},
+		},
 	}
 }
 
 func (this *WordDictionary) AddWord(word string) {
 	iter := this.root
 
-	for _, c := range word {
-		if iter.nodes == nil {
-			iter.nodes = map[rune]*TrieNode{}
+	for _, w := range word {
+		if iter.children == nil {
+			iter.children = map[rune]*TrieNode{}
 		}
-		if _, ok := iter.nodes[c]; !ok {
-			iter.nodes[c] = &TrieNode{}
+		if _, ok := iter.children[w]; !ok {
+			iter.children[w] = &TrieNode{}
 		}
-		iter = iter.nodes[c]
+		iter = iter.children[w]
 	}
 
-	if iter.nodes == nil {
-		iter.nodes = map[rune]*TrieNode{}
-	}
-	if _, ok := iter.nodes['#']; !ok {
-		iter.nodes['#'] = &TrieNode{}
-	}
+	iter.end = true
 }
 
 func (this *WordDictionary) Search(word string) bool {
 	iter := []*TrieNode{this.root}
 
-	for _, c := range word {
-		candidates := make([]*TrieNode, 0)
+	for _, w := range word {
+		if len(iter) == 0 {
+			return false
+		}
 
-		for _, i := range iter {
-			if i.nodes == nil {
-				continue
-			}
-			if c == '.' {
-				for k, n := range i.nodes {
-					if k != '#' {
-						candidates = append(candidates, n)
-					}
+		nextIter := make([]*TrieNode, 0)
+
+		for _, it := range iter {
+			if w == '.' {
+				for _, ch := range it.children {
+					nextIter = append(nextIter, ch)
 				}
 				continue
 			}
-			if _, ok := i.nodes[c]; ok {
-				candidates = append(candidates, i.nodes[c])
+			if _, ok := it.children[w]; ok {
+				nextIter = append(nextIter, it.children[w])
 				continue
 			}
 		}
 
-		iter = candidates
+		iter = nextIter
 	}
 
-	for _, i := range iter {
-		if len(i.nodes) == 0 {
-			continue
-		}
-		if _, ok := i.nodes['#']; ok {
+	for _, it := range iter {
+		if it.end {
 			return true
 		}
 	}
