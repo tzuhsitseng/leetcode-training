58690896ffff275f6b0c6cef7224e0f0a2804770
christseng <tzuhsi.tseng@gmail.com>
2022-05-21 16:59:13 +0800

    feat: resolve the problem that searching the 2D matrix

--- search-a-2d-matrix/main.go
+++ search-a-2d-matrix/main.go
@@ -5,39 +5,35 @@ import "fmt"
 // https://leetcode.com/problems/search-a-2d-matrix/
 
 func searchMatrix(matrix [][]int, target int) bool {
-	if len(matrix) == 0 || len(matrix[0]) == 0 {
-		return false
-	}
+	l, r := 0, len(matrix)-1
+	row := -1
 
-	top, bot := 0, len(matrix)-1
-
-	for top <= bot {
-		mid := (bot + top) / 2
-		if matrix[mid][0] == target {
-			return true
-		} else if matrix[mid][0] < target {
-			top = mid + 1
+	for l <= r {
+		m := (l + r) / 2
+		if target >= matrix[m][0] && target <= matrix[m][len(matrix[m])-1] {
+			row = m
+			break
+		} else if target < matrix[m][0] {
+			r = m - 1
 		} else {
-			bot = mid - 1
+			l = m + 1
 		}
 	}
 
-	row := bot
-	if row < 0 {
+	if row == -1 {
 		return false
 	}
 
-	left, right := 0, len(matrix[0])-1
+	l, r = 0, len(matrix[row])-1
 
-	for left <= right {
-		mid := (left + right) / 2
-
-		if matrix[row][mid] == target {
+	for l <= r {
+		m := (l + r) / 2
+		if target == matrix[row][m] {
 			return true
-		} else if matrix[row][mid] < target {
-			left = mid + 1
+		} else if target < matrix[row][m] {
+			r = m - 1
 		} else {
-			right = mid - 1
+			l = m + 1
 		}
 	}
 
@@ -45,7 +41,7 @@ func searchMatrix(matrix [][]int, target int) bool {
 }
 
 func main() {
-	//fmt.Println(searchMatrix([][]int{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 3))
+	fmt.Println(searchMatrix([][]int{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 3))
 	//fmt.Println(searchMatrix([][]int{{1, 3}}, 3))
-	fmt.Println(searchMatrix([][]int{{1}}, 0))
+	//fmt.Println(searchMatrix([][]int{{1}}, 0))
 }
