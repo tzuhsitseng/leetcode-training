d94ee436f29d470415745cdf207d86d0ff144fbd
christseng <tzuhsi.tseng@gmail.com>
2022-05-21 16:21:50 +0800

    feat: resolve the problem that searching the ascending-ordered array

--- binary-search/main.go
+++ binary-search/main.go
@@ -3,26 +3,16 @@ package main
 // https://leetcode.com/problems/binary-search/
 
 func search(nums []int, target int) int {
-	size := len(nums)
+	l, r := 0, len(nums)-1
 
-	if size == 0 {
-		return -1
-	}
-
-	if size == 1 && nums[0] == target {
-		return 0
-	}
-
-	l, r := 0, size-1
-
-	for r >= l {
-		m := (r-l)/2 + l
-		if target == nums[m] {
+	for l <= r {
+		m := (l + r) / 2
+		if nums[m] == target {
 			return m
-		} else if target > nums[m] {
-			l = m + 1
-		} else {
+		} else if nums[m] > target {
 			r = m - 1
+		} else {
+			l = m + 1
 		}
 	}
 
