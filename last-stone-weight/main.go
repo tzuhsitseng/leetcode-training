b060006f31754ef53ede2639abc62db82c7dd26d
christseng <tzuhsi.tseng@gmail.com>
2022-05-23 15:42:39 +0800

    feat: resolve the problem that finding the last stone weight

--- last-stone-weight/main.go
+++ last-stone-weight/main.go
@@ -1,15 +1,21 @@
 package main
 
-import "container/heap"
+import (
+	"container/heap"
+	"math"
+)
 
 // https://leetcode.com/problems/last-stone-weight/
 
 type IntHeap []int
 
-func (h IntHeap) Len() int            { return len(h) }
-func (h IntHeap) Less(i, j int) bool  { return h[i] > h[j] }
-func (h IntHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
-func (h *IntHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
 func (h *IntHeap) Pop() interface{} {
 	size := len(*h)
 	res := (*h)[size-1]
@@ -18,27 +24,23 @@ func (h *IntHeap) Pop() interface{} {
 }
 
 func lastStoneWeight(stones []int) int {
-	if len(stones) == 0 {
-		return 0
-	}
 	if len(stones) == 1 {
 		return stones[0]
 	}
 
-	hp := IntHeap(stones)
-	heap.Init(&hp)
+	h := IntHeap(stones)
+	heap.Init(&h)
 
-	for hp.Len() > 1 {
-		p1 := heap.Pop(&hp).(int)
-		p2 := heap.Pop(&hp).(int)
-		if p1-p2 > 0 {
-			heap.Push(&hp, p1-p2)
+	for h.Len() >= 2 {
+		s1 := heap.Pop(&h).(int)
+		s2 := heap.Pop(&h).(int)
+		if s1 != s2 {
+			heap.Push(&h, int(math.Abs(float64(s1)-float64(s2))))
 		}
 	}
 
-	if hp.Len() == 1 {
-		return hp[0]
+	if h.Len() > 0 {
+		return h[0]
 	}
-
 	return 0
 }
