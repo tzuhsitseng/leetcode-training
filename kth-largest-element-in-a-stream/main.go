185cca32bf073a064de1bcb9d7da2280ec02e5fa
christseng <tzuhsi.tseng@gmail.com>
2022-05-23 15:28:16 +0800

    feat: resolve the problem that finding the kth largest element

--- kth-largest-element-in-a-stream/main.go
+++ kth-largest-element-in-a-stream/main.go
@@ -9,41 +9,56 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int            { return len(h) }
-func (h IntHeap) Less(i, j int) bool  { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
-func (h *IntHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
 func (h *IntHeap) Pop() interface{} {
-	size := len(*h)
-	res := (*h)[size-1]
-	*h = (*h)[:size-1]
+	l := h.Len()
+	res := (*h)[l-1]
+	*h = (*h)[:l-1]
 	return res
 }
 
 type KthLargest struct {
-	k  int
-	hp IntHeap
+	kth  int
+	data *IntHeap
 }
 
 func Constructor(k int, nums []int) KthLargest {
-	hp := IntHeap(nums)
-	heap.Init(&hp)
+	input := IntHeap(nums)
+	heap.Init(&input)
 
 	for i := 0; i < len(nums)-k; i++ {
-		heap.Pop(&hp)
+		_ = heap.Pop(&input)
 	}
 
-	return KthLargest{k, hp}
+	return KthLargest{
+		kth:  k,
+		data: &input,
+	}
 }
 
 func (this *KthLargest) Add(val int) int {
-	heap.Push(&this.hp, val)
+	heap.Push(this.data, val)
 
-	if this.hp.Len() > this.k {
-		heap.Pop(&this.hp)
+	if this.data.Len() > this.kth {
+		_ = heap.Pop(this.data)
 	}
 
-	return this.hp[0]
+	return (*this.data)[0]
 }
 
 func main() {
