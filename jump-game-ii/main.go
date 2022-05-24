299c1687451c579aaba3a6cddc2f50e5c2be9336
christseng <tzuhsi.tseng@gmail.com>
2022-05-24 23:15:46 +0800

    feat: resolve the problem that playing the jump game ii

--- jump-game-ii/main.go
+++ jump-game-ii/main.go
@@ -10,18 +10,15 @@ func max(a, b int) int {
 }
 
 func jump(nums []int) int {
-	if len(nums) == 0 {
-		return 0
-	}
-
-	res := 0
 	furthest := 0
 	l, r := 0, 0
+	res := 0
 
 	for r < len(nums)-1 {
 		for i := l; i <= r; i++ {
-			furthest = max(furthest, i+nums[i])
+			furthest = max(furthest, nums[i]+i)
 		}
+
 		l = r + 1
 		r = furthest
 		res++
