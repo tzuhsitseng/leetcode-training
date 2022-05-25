a2224f5dab1a81638cde864ebeef8393e146ebb5
christseng <tzuhsi.tseng@gmail.com>
2022-05-25 14:10:51 +0800

    feat: resolve the problem that finding the distinct ways to climbing stairs

--- climbing-stairs/main.go
+++ climbing-stairs/main.go
@@ -3,15 +3,15 @@ package main
 // https://leetcode.com/problems/climbing-stairs/
 
 func climbStairs(n int) int {
-	if n <= 2 {
-		return n
+	if n == 1 {
+		return 1
 	}
 
 	dp := make([]int, n)
 	dp[0] = 1
 	dp[1] = 2
 
-	for i := 2; i <= n-1; i++ {
+	for i := 2; i < n; i++ {
 		dp[i] = dp[i-1] + dp[i-2]
 	}
 
