ff7ee3ccdd7fb71ac19fc0088e2fb46346e9b62b
christseng <tzuhsi.tseng@gmail.com>
2022-05-26 13:39:17 +0800

    feat: resolve the problem that finding all decoding ways

--- decode-ways/main.go
+++ decode-ways/main.go
@@ -1,33 +1,39 @@
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
 // https://leetcode.com/problems/decode-ways/
 
 func numDecodings(s string) int {
-	dp := map[int]int{
-		len(s): 1,
-	}
+	dp := map[int]int{}
 
-	var dfs func(i int) int
-	dfs = func(i int) int {
+	var dfs func(idx int) int
+	dfs = func(idx int) int {
 		res := 0
 
-		if v, ok := dp[i]; ok {
+		if v, ok := dp[idx]; ok {
 			return v
 		}
 
-		if s[i] == '0' {
+		if idx == len(s) {
+			return 1
+		}
+
+		if s[idx] == '0' {
 			return 0
 		}
 
-		res = dfs(i + 1)
-		if i < len(s)-1 &&
-			(s[i] == '1' || s[i] == '2' &&
-				strings.Index("0123456", string(s[i+1])) != -1) {
-			res += dfs(i + 2)
+		res += dfs(idx + 1)
+
+		if idx < len(s)-1 &&
+			(s[idx] == '1' ||
+				(s[idx] == '2' && strings.Contains("0123456", string(s[idx+1])))) {
+			res += dfs(idx + 2)
 		}
-		dp[i] = res
+
+		dp[idx] = res
 		return res
 	}
 
