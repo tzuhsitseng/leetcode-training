e0adb90d20e2afeb2d222eebd29bd53fa50ff9b6
christseng <tzuhsi.tseng@gmail.com>
2022-06-09 23:59:46 +0800

    feat: resolve the problem that finding the longest common sequence

--- longest-common-subsequence/main.go
+++ longest-common-subsequence/main.go
@@ -2,6 +2,28 @@ package main
 
 // https://leetcode.com/problems/longest-common-subsequence/
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func longestCommonSubsequence(text1 string, text2 string) int {
-	return 0
+	dp := make([][]int, len(text1)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(text2)+1)
+	}
+
+	for i := len(text1) - 1; i >= 0; i-- {
+		for j := len(text2) - 1; j >= 0; j-- {
+			if text1[i] == text2[j] {
+				dp[i][j] = 1 + dp[i+1][j+1]
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+			}
+		}
+	}
+
+	return dp[0][0]
 }
