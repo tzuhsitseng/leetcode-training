116cbdf65b4e1542dd81cc78d3a5ad3564662b6f
christseng <tzuhsi.tseng@gmail.com>
2022-06-22 14:31:31 +0800

    feat: resolve the problem that finding the sum of three elements equals to 0

--- 3sum/main.go
+++ 3sum/main.go
@@ -1,42 +1,45 @@
 package main
 
+// https://leetcode.com/problems/3sum/
+
 import "sort"
 
 func threeSum(nums []int) [][]int {
-	result := make([][]int, 0)
 	l := len(nums)
 
-	// 先排序
+	if l == 0 {
+		return nil
+	}
+
+	res := make([][]int, 0)
 	sort.Ints(nums)
 
-	// 開始使用 two pointers 去查訪
 	for i := 0; i < l-2; i++ {
-		if i == 0 || (i > 0 && nums[i] != nums[i-1]) {
-			j := i + 1
-			k := l - 1
-			target := 0 - nums[i]
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
 
-			for j < k {
-				nj := nums[j]
-				nk := nums[k]
-				if nj+nk == target {
-					result = append(result, []int{nums[i], nums[j], nums[k]})
+		j := i + 1
+		k := l - 1
+		target := 0 - nums[i]
 
-					for j < k && nums[j] <= nj {
-						j++
-					}
-
-					for j < k && nums[k] >= nk {
-						k--
-					}
-				} else if nj+nk < target {
+		for j < k {
+			numJ, numK := nums[j], nums[k]
+			if target == numJ+numK {
+				res = append(res, []int{nums[i], nums[j], nums[k]})
+				for j < k && nums[j] <= numJ {
 					j++
-				} else if nj+nk > target {
+				}
+				for j < k && nums[k] >= numK {
 					k--
 				}
+			} else if target < numJ+numK {
+				k--
+			} else {
+				j++
 			}
 		}
 	}
 
-	return result
+	return res
 }
